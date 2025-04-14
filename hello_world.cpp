7d0d5d61fc94c3000e4f0c407fb7375782e60347
edikcarlos <edikcarlos@hotmail.com>
2025-04-14 20:37:38 -0300

    iniciando curso

---
+++ hello_world.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int main() {
+    cout << "Hello, World2!" << endl;
+    system("pause");
+    return 0;
+}
---
+++ soma.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num = 10;
+    int num2 = 33;
+    int soma = num + num2;
+    
+    cout << soma << endl;
+    return 0;
+}
